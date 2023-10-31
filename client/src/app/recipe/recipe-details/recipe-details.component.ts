f1e89739f9bb51f7a393a31a8f6dc8e95630cf24
houkii <jastrzebski.j.m@gmail.com>
2023-10-31 15:26:49 +0100

    Added images viewing using lightbox for recipe details

--- client/src/app/recipe/recipe-details/recipe-details.component.ts
+++ client/src/app/recipe/recipe-details/recipe-details.component.ts
@@ -4,14 +4,14 @@ import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { RecipesService } from 'src/app/cooker/recipes.service';
 import { Recipe } from 'src/app/models/recipe';
-import { RecipeStep } from 'src/app/models/recipeStep';
+import { IAlbum, Lightbox, LightboxConfig, LightboxModule } from 'ngx-lightbox';
 
 @Component({
   selector: 'app-recipe-details',
   templateUrl: './recipe-details.component.html',
   styleUrls: ['./recipe-details.component.scss'],
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LightboxModule ],
   animations: [
     trigger('openClose', [
       state('closed', style({ transform: 'translateX(-100%)', opacity: 0})),
@@ -27,8 +27,13 @@ import { RecipeStep } from 'src/app/models/recipeStep';
 export class RecipeDetailsComponent implements OnInit {
   active : boolean = false;
   recipe?: Recipe;
+  albums: IAlbum[] = [];
 
-  constructor(private recipeService: RecipesService, private activatedRoute: ActivatedRoute, private router: Router) {}
+  constructor(private recipeService: RecipesService, private activatedRoute: ActivatedRoute, 
+    private router: Router, private lightbox : Lightbox, private lightboxConfig : LightboxConfig) {
+      lightboxConfig.resizeDuration = .35;
+      lightboxConfig.fadeDuration = .35;
+  }
 
   ngOnInit(): void {
     const id = this.activatedRoute.snapshot.paramMap.get('id');
@@ -37,11 +42,28 @@ export class RecipeDetailsComponent implements OnInit {
     this.recipeService.getRecipe(+id).subscribe({
       next: data => {
         this.recipe = data;
+        this.generateAlbums();
       }
-    })
+    });
   }
 
   onClickBackButton() {
     this.router.navigateByUrl("/cook");
+  }
+
+  open(index: number): void {
+    this.lightbox.open(this.albums, index);
+  }
+
+  generateAlbums() {
+    this.recipe?.pictureUrls.forEach((src, index) => {
+      const album = {
+        src: src,
+        caption: 'Image ' + (index + 1),
+        thumb: src
+      };
+
+      this.albums.push(album);
+    });
   }
 }
