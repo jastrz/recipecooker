b60809667838ce442703d9f196428ec2760ab6b3
houkii <jastrzebski.j.m@gmail.com>
2023-11-06 12:13:29 +0100

    Added album component, changed recipe details styling

---
+++ client/src/app/common/album/album.component.ts
@@ -0,0 +1,46 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { IAlbum, Lightbox, LightboxConfig, LightboxModule } from 'ngx-lightbox';
+
+@Component({
+  selector: 'app-album',
+  templateUrl: './album.component.html',
+  styleUrls: ['./album.component.scss'],
+  standalone: true,
+  imports: [CommonModule, LightboxModule],
+})
+export class AlbumComponent implements OnInit {
+  @Input() pictureUrls: string[] = [];
+  albums: IAlbum[] = [];
+
+  constructor(
+    private lightbox: Lightbox,
+    private lightboxConfig: LightboxConfig
+  ) {
+    lightboxConfig.resizeDuration = 0.35;
+    lightboxConfig.fadeDuration = 0.35;
+    lightboxConfig.fitImageInViewPort = true;
+    lightboxConfig.centerVertically = true;
+    lightboxConfig.disableScrolling = true;
+  }
+
+  ngOnInit(): void {
+    this.generateAlbums();
+  }
+
+  open(index: number): void {
+    this.lightbox.open(this.albums, index);
+  }
+
+  private generateAlbums() {
+    this.pictureUrls.forEach((src, index) => {
+      const album = {
+        src: src,
+        caption: 'Image ' + (index + 1),
+        thumb: src,
+      };
+
+      this.albums.push(album);
+    });
+  }
+}
--- client/src/app/recipe/recipe-details/recipe-details.component.ts
+++ client/src/app/recipe/recipe-details/recipe-details.component.ts
@@ -1,10 +1,16 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { CommonModule } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AlbumComponent } from 'src/app/common/album/album.component';
 import { RecipesService } from 'src/app/cooker/recipes.service';
 import { Recipe } from 'src/app/models/recipe';
-import { IAlbum, Lightbox, LightboxConfig, LightboxModule } from 'ngx-lightbox';
 import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
@@ -12,66 +18,41 @@ import { BreadcrumbService } from 'xng-breadcrumb';
   templateUrl: './recipe-details.component.html',
   styleUrls: ['./recipe-details.component.scss'],
   standalone: true,
-  imports: [CommonModule, LightboxModule ],
+  imports: [CommonModule, AlbumComponent],
   animations: [
     trigger('openClose', [
-      state('closed', style({ transform: 'translateX(-100%)', opacity: 0})),
-      transition('open => closed', [
-        animate('.1s ease-out')
-      ]),
-      transition('closed => open', [
-        animate(('.15s ease-in'))
-      ])
-    ])
-  ]
+      state('closed', style({ transform: 'translateX(-100%)', opacity: 0 })),
+      transition('open => closed', [animate('.1s ease-out')]),
+      transition('closed => open', [animate('.15s ease-in')]),
+    ]),
+  ],
 })
 export class RecipeDetailsComponent implements OnInit {
-  active : boolean = false;
+  active: boolean = false;
   recipe?: Recipe;
-  albums: IAlbum[] = [];
 
-  constructor(private recipeService: RecipesService, private activatedRoute: ActivatedRoute, 
-    private router: Router, private lightbox : Lightbox, private lightboxConfig : LightboxConfig,
-    private breadcrumbService : BreadcrumbService) {
-      lightboxConfig.resizeDuration = .35;
-      lightboxConfig.fadeDuration = .35;
-      lightboxConfig.fitImageInViewPort = true;
-      lightboxConfig.centerVertically = true;
-      lightboxConfig.disableScrolling = true;
-
-      this.breadcrumbService.set('@recipe', ' ');
+  constructor(
+    private recipeService: RecipesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private breadcrumbService: BreadcrumbService
+  ) {
+    this.breadcrumbService.set('@recipe', ' ');
   }
 
   ngOnInit(): void {
     const id = this.activatedRoute.snapshot.paramMap.get('id');
-    if(!id) return;
+    if (!id) return;
 
     this.recipeService.getRecipe(+id).subscribe({
-      next: data => {
+      next: (data) => {
         this.recipe = data;
         this.breadcrumbService.set('@recipe', this.recipe.name);
-        this.generateAlbums();
-      }
+      },
     });
   }
 
   onClickBackButton() {
-    this.router.navigateByUrl("/cook");
-  }
-
-  open(index: number): void {
-    this.lightbox.open(this.albums, index);
-  }
-
-  generateAlbums() {
-    this.recipe?.pictureUrls.forEach((src, index) => {
-      const album = {
-        src: src,
-        caption: 'Image ' + (index + 1),
-        thumb: src
-      };
-
-      this.albums.push(album);
-    });
+    this.router.navigateByUrl('/cook');
   }
 }
