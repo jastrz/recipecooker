c98d3458199a9e821a1b5c107470db481ac3b16b
houkii <jastrzebski.j.m@gmail.com>
2023-11-09 16:43:06 +0100

    Navbar account button fix

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { AccountService } from '../services/account.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent {
-
+  constructor(public accountService: AccountService) {}
 }
